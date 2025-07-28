ef232fb5fad5cb7d56dfb87d92d56cd3d66f2c23
Abel Tomy <abeltomy05@gmail.com>
2025-07-28 21:19:37 +0530

    fixed id proof validation

--- src/shared/validations/idProof.validation.ts
+++ src/shared/validations/idProof.validation.ts
@@ -2,8 +2,4 @@ import { z } from "zod";
 
 export const idProofSchema = z
   .string()
-  .url({ message: "Invalid URL format for ID proof." })
-  .refine(url =>
-    /^[a-zA-Z0-9/_\-]+\.(jpg|jpeg|png|pdf|docx)$/i.test(url), {
-      message: "ID proof must be a valid file URL.",
-  });
+  .min(1, "ID proof is required")
