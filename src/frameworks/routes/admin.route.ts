d221334727fd3eb349b86e565689eeac43f35298
Abel Tomy <abeltomy05@gmail.com>
2025-08-06 11:20:12 +0530

    Changed  api url to proper standard

--- src/frameworks/routes/admin.route.ts
+++ src/frameworks/routes/admin.route.ts
@@ -9,75 +9,75 @@ export class AdminRoutes extends BaseRoute{
     }
 
     protected initializeRoutes(): void {
-          this.router.post('/admin/logout',verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+          this.router.post('/logout',verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             authController.logout(req, res, next);
          })
 
-         this.router.post("/admin/refresh-token",(req: Request, res: Response) => {
+         this.router.post("/refresh-token",(req: Request, res: Response) => {
             authController.handleTokenRefresh(req, res);
         });
 
-        this.router.get("/admin/getAllUsers",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/getAllUsers",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             usersController.getAllUsers(req, res, next);
         });
 
-         this.router.patch("/admin/update-status",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+         this.router.patch("/update-status",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             usersController.updateEntityStatus(req, res, next);
         });
-        this.router.get("/admin/get-user-count",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/get-user-count",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             usersController.getUserCount(req, res, next);
         });
 
-         this.router.get("/admin/get-pending-buildings",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+         this.router.get("/get-pending-buildings",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             buildingController.getBuildingsForVerification(req, res, next);
         });
 
-          this.router.get("/admin/get-wallet-details",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+          this.router.get("/get-wallet-details",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             walletController.getWalletDetails(req, res, next);
         });
-         this.router.get("/admin/get-bookings",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+         this.router.get("/get-bookings",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             bookingController.getBookingsForAdmin(req, res, next);
         });
-        this.router.get("/admin/get-vendor-buildings",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/get-vendor-buildings",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             usersController.getVendorsAndBuildings(req, res, next);
         });
-        this.router.get("/admin/get-single-vendor/:vendorId",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/get-single-vendor/:vendorId",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             vendorController.singleVendorData(req, res, next);
         });
-        this.router.get("/admin/get-notifications",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/get-notifications",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             notifiactionController.getNotifications(req, res, next);
         });
-        this.router.patch("/admin/mark-as-read/:id",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.patch("/mark-as-read/:id",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             notifiactionController.markAsRead(req, res, next);
         });
-         this.router.patch("/admin/mark-as-read",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+         this.router.patch("/mark-as-read",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             notifiactionController.markAsRead(req, res, next);
         });
-        this.router.get("/admin/monthly-stats",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/monthly-stats",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             usersController.getMonthlyBookingStats(req, res, next);
         });
-        this.router.get("/admin/get-every-building",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/get-every-building",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             buildingController.getEveryBuilding(req, res, next);
         });
-        this.router.get("/admin/revenue-chart",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/revenue-chart",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             bookingController.getRevenueChartData(req, res, next);
         });
-        this.router.get("/admin/revenue-report",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/revenue-report",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             bookingController.adminRevenueReport(req, res, next);
         });
-        this.router.delete("/admin/clear-notifications",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.delete("/clear-notifications",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             notifiactionController.clearNotification(req, res, next);
         });
-        this.router.get("/admin/get-amenities",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/get-amenities",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             amenityController.getAllAmenity(req, res, next);
         });
-        this.router.post("/admin/create-amenity",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.post("/create-amenity",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             amenityController.createAmenity(req, res, next);
         });
-        this.router.patch("/admin/edit-amenity",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.patch("/edit-amenity",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             amenityController.editAmenity(req, res, next);
         });
-        this.router.delete("/admin/delete-amenity",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
+        this.router.delete("/delete-amenity",verifyAuth, authorizeRole(["admin"]), (req: Request, res: Response, next: NextFunction) => {
             amenityController.deleteAmenity(req, res, next);
         });
     }
--- src/frameworks/routes/client.route.ts
+++ src/frameworks/routes/client.route.ts
@@ -10,96 +10,96 @@ export class ClientRoutes extends BaseRoute{
 
     protected initializeRoutes(): void {
 
-        this.router.post('/client/logout',verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.post('/logout',verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             authController.logout(req, res, next);
          })
 
-          this.router.post("/client/refresh-token",decodeToken,(req: Request, res: Response) => {
+          this.router.post("/refresh-token",decodeToken,(req: Request, res: Response) => {
             authController.handleTokenRefresh(req, res);
          });
 
-         this.router.get("/client/get-user-data", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+         this.router.get("/get-user-data", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             usersController.getUserData(req, res, next);
         });
 
-        this.router.patch("/client/update-profile",verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.patch("/update-profile",verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             usersController.updateUserProfile(req, res, next);
         });
 
-        this.router.patch("/client/update-password", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.patch("/update-password", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             usersController.updateUserPassword(req, res, next);
         });
 
-         this.router.get("/client/list-buildings", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+         this.router.get("/list-buildings", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             buildingController.fetchBuildings(req, res, next);
         });
 
-         this.router.get("/client/building/:id", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+         this.router.get("/building/:id", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             buildingController.getSingleBuilding(req, res, next);
         });
 
-        this.router.get("/client/get-booking-page-data/:spaceId", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/get-booking-page-data/:spaceId", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             bookingController.getBookingPageData(req, res, next);
         });
 
-        this.router.post("/client/create-payment-intent", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.post("/create-payment-intent", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             bookingController.createPaymentIntent(req, res, next);
         });
 
-        this.router.post("/client/confirm-payment", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.post("/confirm-payment", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             bookingController.confirmPayment(req, res, next);
         });
 
-        this.router.get("/client/get-bookings", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/get-bookings", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             bookingController.getBookings(req, res, next);
         });
-         this.router.get("/client/get-booking-details/:bookingId", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+         this.router.get("/get-booking-details/:bookingId", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             bookingController.getBookingDetails(req, res, next);
         });
 
-        this.router.patch("/client/cancel-booking", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.patch("/cancel-booking", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             bookingController.cancelBooking(req, res, next);
         });
-        this.router.get("/client/get-wallet-details", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/get-wallet-details", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             walletController.getWalletDetails(req, res, next);
         });
-        this.router.post("/client/pay-with-wallet", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.post("/pay-with-wallet", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             walletController.payWithWallet(req, res, next);
         });
-        this.router.post("/client/create-topup-payment-intent", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.post("/create-topup-payment-intent", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             walletController.createTopupIntent(req, res, next);
         });
-         this.router.post("/client/confirm-topup-payment", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+         this.router.post("/confirm-topup-payment", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             walletController.confirmTopupPayment(req, res, next);
         });
-         this.router.get("/client/get-notifications", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+         this.router.get("/get-notifications", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             notifiactionController.getNotifications(req, res, next);
         });
-        this.router.patch("/client/mark-as-read/:id", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.patch("/mark-as-read/:id", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             notifiactionController.markAsRead(req, res, next);
         });
-        this.router.patch("/client/mark-as-read", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.patch("/mark-as-read", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             notifiactionController.markAsRead(req, res, next);
         });
-        this.router.post("/client/create-session", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.post("/create-session", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             chatController.createSession(req, res, next);
         });
-        this.router.get("/client/getChats", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/getChats", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             chatController.getChats(req, res, next);
         });
-        this.router.get("/client/messages", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/messages", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             chatController.getMessages(req, res, next);
         });
-        this.router.patch("/client/clear-chat", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.patch("/clear-chat", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             chatController.clearChat(req, res, next);
         });
-        this.router.get("/client/fetch-filters", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/fetch-filters", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             buildingController.fetchFilters(req, res, next);
         });
-        this.router.delete("/client/clear-notifications", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.delete("/clear-notifications", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             notifiactionController.clearNotification(req, res, next);
         });
-        this.router.get("/client/get-amenities", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
+        this.router.get("/get-amenities", verifyAuth, authorizeRole(["client"]), blockStatusMiddleware.checkStatus as RequestHandler, (req: Request, res: Response, next: NextFunction) => {
             amenityController.getAllAmenity(req, res, next);
         });
     }
